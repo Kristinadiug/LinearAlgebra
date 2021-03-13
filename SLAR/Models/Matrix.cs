525ca95febb12be78d7ca7503804d280f2a143ce
Kristinadiug <kristina.diug@gmail.com>
2021-03-13 14:58:07 +0200

    first commit

---
+++ SLAR/Models/Matrix.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAR.Models
+{
+    public class Matrix
+    {
+        int n { get; set; }
+        int m { get; set; }
+
+        private double[][] matrix;
+
+        public Matrix()
+        {
+            this.n = 1;
+            this.m = 1;
+        }
+
+        public Matrix(int _n, int _m)
+        {
+            this.n = _n;
+            this.m = _m;
+            Initialize();
+        }
+
+        public Matrix(int _n, int _m, double value) : this(_n, _m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i][j] = value;
+                }
+            }
+        }
+
+        private void Initialize()
+        {
+            matrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new double[m];
+            }
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if (a.n != b.n || a.m != b.m)
+            {
+                throw new ArgumentException("Matrixes have to be the same size");
+            }
+
+            Matrix result = new Matrix(a.n, a.m);
+
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < a.m; j++)
+                {
+                    result.matrix[i][j] = a.matrix[i][j] + b.matrix[i][j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a.n != b.n || a.m != b.m)
+            {
+                throw new ArgumentException("Matrixes have to be the same size");
+            }
+
+            Matrix result = new Matrix(a.n, a.m);
+
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < a.m; j++)
+                {
+                    result.matrix[i][j] = a.matrix[i][j] - b.matrix[i][j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.m != b.n)
+            {
+                throw new ArgumentException("Can not multiply matrixes of this size");
+            }
+
+            Matrix result = new Matrix(a.n, b.m, 0);
+            for (int i = 0; i < a.n; i++)
+            {
+                for (int j = 0; j < b.m; j++)
+                {
+                    for (int k = 0; k < b.n; k++)
+                    {
+                        result.matrix[i][j] += a.matrix[i][k] * b.matrix[k][j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+
+        public void ReadFromFile(string path)
+        {
+            string[] text = File.ReadAllText(path).Split();
+            n = Convert.ToInt32(text[0]);
+            m = Convert.ToInt32(text[1]);
+
+            Initialize();
+
+            int index = 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i][j] = Convert.ToDouble(text[index]);
+                    index++;
+                }
+            }
+        }
+
+        public void FillWithRandomValues(double min = 0, double max = 10000000)
+        {
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i][j] = random.NextDouble() * (max - min) + min;
+                }
+            }
+        }
+    }
+}
+
---
+++ SLAR/Models/Vector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAR.Models
+{
+    class Vector: Matrix
+    {
+        public Vector(int n) : base(n, 1) { }
+
+        public Vector() : base() { }
+    }
+}
